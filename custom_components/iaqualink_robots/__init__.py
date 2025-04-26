e59ead77aeafd5b1802e05ef0564c9518ba41a97
Matthew Allen <99111424+mlallen2@users.noreply.github.com>
2025-04-26 03:22:40 -0400

    Update __init__.py

--- custom_components/iaqualink_robots/__init__.py
+++ custom_components/iaqualink_robots/__init__.py
@@ -1,1 +1,27 @@
-"""IaqualinkRobots integration."""
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from .const import DOMAIN
+from .coordinator import IAquaLinkCoordinator
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up integration from a config entry."""
+    coordinator = IAquaLinkCoordinator(hass, entry)
+    await coordinator.async_config_entry_first_refresh()
+    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
+
+    # Forward to each platform
+    for platform in ("vacuum", "sensor", "binary_sensor"):
+        hass.async_create_task(
+            hass.config_entries.async_forward_entry_setup(entry, platform)
+        )
+    return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    unload_ok = all(
+        await hass.config_entries.async_forward_entry_unload(entry, platform)
+        for platform in ("vacuum", "sensor", "binary_sensor")
+    )
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+    return unload_ok
