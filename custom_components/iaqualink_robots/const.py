1a82f0e664f26947b1a12fa4112d8276bbf1edf6
Matthew Allen <99111424+mlallen2@users.noreply.github.com>
2025-04-26 04:14:34 -0400

    Update const.py

--- custom_components/iaqualink_robots/const.py
+++ custom_components/iaqualink_robots/const.py
@@ -3,6 +3,7 @@ import json
 from datetime import timedelta
 from pathlib import Path
 from typing import Final
+
 from homeassistant.const import Platform
 
 PLATFORMS: Final = [
@@ -11,30 +12,28 @@ PLATFORMS: Final = [
     Platform.BINARY_SENSOR,
 ]
 
-
-URL_LOGIN="https://prod.zodiac-io.com/users/v1/login"
-URL_GET_DEVICES="https://r-api.iaqualink.net/devices.json"
-URL_GET_DEVICE_STATUS="https://prod.zodiac-io.com/devices/v1/"
-URL_GET_DEVICE_FEATURES="https://prod.zodiac-io.com/devices/v2/"
+URL_LOGIN             = "https://prod.zodiac-io.com/users/v1/login"
+URL_GET_DEVICES       = "https://r-api.iaqualink.net/devices.json"
+URL_GET_DEVICE_STATUS = "https://prod.zodiac-io.com/devices/v1/"
+URL_GET_DEVICE_FEATURES = "https://prod.zodiac-io.com/devices/v2/"
 
 SCAN_INTERVAL = timedelta(seconds=30)
 
-manifestfile = Path(__file__).parent / "manifest.json"
+# Load manifest values
+manifestfile   = Path(__file__).parent / "manifest.json"
 with open(manifestfile) as json_file:
     manifest_data = json.load(json_file)
 
-DOMAIN = manifest_data.get("domain")
-NAME = manifest_data.get("name")
-VERSION = manifest_data.get("version")
-ISSUEURL = manifest_data.get("issue_tracker")
-STARTUP = """
+DOMAIN    = manifest_data["domain"]
+NAME      = manifest_data["name"]
+VERSION   = manifest_data["version"]
+ISSUEURL  = manifest_data.get("issue_tracker", "")
+STARTUP   = f"""
 -------------------------------------------------------------------
-{name}
-Version: {version}
+{NAME}
+Version: {VERSION}
 This is a custom component
-If you have any issues with this you need to open an issue here:
-{issueurl}
+If you have any issues, open one here:
+{ISSUEURL}
 -------------------------------------------------------------------
-""".format(
-    name=NAME, version=VERSION, issueurl=ISSUEURL
-)
+"""
